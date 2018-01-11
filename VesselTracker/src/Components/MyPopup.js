f5201a71077e6ec8337ff116eeca5c5e89cdacdf
mixos <mkourogiorgas@gmail.com>
2018-01-11 22:24:59 +0200

    popup component

---
+++ VesselTracker/src/Components/MyPopup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Popup } from 'react-leaflet';
+import PopupItem from './PopupItem.js';
+import { Status, Time } from './Calculations.js'
+
+class MyPopup extends React.Component {
+	render() {
+		return (
+			<Popup className='popup'>
+				<div>
+					<PopupItem title={'Mssi: '} value={this.props.popupInfo.mssi} icon={'Mssi'}/>
+					<PopupItem title={'Time: '} value={Time(this.props.popupInfo.timestamp)} icon={'Time'}/>
+					<PopupItem title={'Status: '} value={Status(this.props.popupInfo.status)} icon={'Status'}/>
+					<PopupItem title={'Speed: '} value={this.props.popupInfo.speed} icon={'Velocity'}/>
+				</div>
+			</Popup>
+		)
+	}
+}
+export default MyPopup;
